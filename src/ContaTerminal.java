441fc746d198f49c7cb28134a094c15d6bd2ac75
Matheusevangelista99 <matheuscharnas@gmail.com>
2024-05-20 17:12:45 -0300

    Projeto criado

---
+++ src/ContaTerminal.java
@@ -0,0 +1,27 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ContaTerminal {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Por favor, digite o número da Agência!");
+		System.out.print("Numero: ");
+		int numero = sc.nextInt();
+		System.out.print("Agencia: ");
+		String agencia = sc.next();
+		System.out.print("Nome: ");
+		String nomeCliente = sc.nextLine();
+		sc.nextLine();
+		System.out.print("Saldo: ");
+		double saldo = sc.nextDouble();
+
+		System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é: "
+				+ agencia + ", conta: " + numero + " e seu saldo: " + saldo + " já está disponível para saque");
+
+	}
+
+}
